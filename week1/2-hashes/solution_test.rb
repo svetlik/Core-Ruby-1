66bf57ae47219fd1439b6ac3e3696c4f2da41ef9
svetlozaurus <svetlik@gmail.com>
2014-10-21 21:17:03 +0300

    Fix checks for mutated methods

--- week1/2-hashes/solution_test.rb
+++ week1/2-hashes/solution_test.rb
@@ -23,15 +23,40 @@ class HashTest < Minitest::Test
     assert_equal({ a: 1, b: 2, c: 3 }.pick!(:a, :b), a: 1, b: 2)
   end
 
+  def test_pick_mutates
+    hash = { a: 1, b: 2, c: 3 }
+    hash.pick!(:a, :b)
+
+    assert_equal({ a: 1, b: 2 }, hash)
+  end
+
   def test_except!
     assert_equal({ a: 1, b: 2, d: nil }.except!(:d), a: 1, b: 2)
+  end
+
+  def test_except_mutates
+    hash = { a: 1, b: 2, d: nil }
+    hash.except!(:d)
+    assert_equal({ a: 1, b: 2 }, hash)
   end
 
   def test_compact_values!
     assert_equal({ a: 1, b: 2, c: false, d: nil }.compact_values!, a: 1, b: 2)
   end
 
+  def test_compact_values_mutates
+    hash = { a: 1, b: 2, c: false, d: nil }
+    hash.compact_values!
+    assert_equal({ a: 1, b: 2 }, hash)
+  end
+
   def test_defaults!
     assert_equal({ a: 1, b: 2 }.defaults!(a: 4, c: 3), a: 1, b: 2, c: 3)
+  end
+
+  def test_defaults_mutates
+    hash = { a: 1, b: 2 }
+    hash.defaults!(a: 4, c: 3)
+    assert_equal({ a: 1, b: 2, c: 3 }, hash)
   end
 end
