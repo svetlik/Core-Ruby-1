5cd2de6b332a6bf0a7db8828ec531d4941877098
svetlozaurus <svetlik@gmail.com>
2014-11-25 23:04:47 +0200

    Refactor Standard Types solutions

--- week0/1-standard-types/solution.rb
+++ week0/1-standard-types/solution.rb
@@ -15,8 +15,10 @@ end
 def ordinal(n)
   ones = n % 10
   tens = n % 100
-  if (4..20).include? tens then "#{n}th"
-  else choose_one(n, ones)
+  if (4..20).include? tens
+    "#{n}th"
+  else
+    choose_one(n, ones)
   end
 end
 
@@ -31,19 +33,23 @@ end
 
 def palindrome?(object)
   string = object.to_s.downcase.gsub(/\s+/, '')
-  string == string.reverse ? true : false
+  string == string.reverse
 end
 
 def anagram?(word, other)
-  histogram(word) == histogram(other) ? true : false
+  histogram(word) == histogram(other)
 end
 
 def remove_prefix(string, prefix)
-  string.slice(prefix.length..string.length)
+  string
+    .slice(prefix.length..string.length) if string.start_with? prefix
 end
 
 def remove_suffix(string, suffix)
-  string.reverse!.slice(suffix.length..string.length).reverse!
+  string
+    .reverse!
+    .slice(suffix.length..string.length)
+    .reverse! if string.end_with? suffix
 end
 
 def digits(n)
@@ -53,10 +59,10 @@ end
 def fizzbuzz(range)
   array = []
   range.each do |n|
-    if (n % 15 == 0) then array << :fizzbuzz
-    elsif (n % 5 == 0) then array << :buzz
-    elsif (n % 3 == 0) then array << :fizz
-    else array << n
+    if    (n % 15 == 0) then array << :fizzbuzz
+    elsif (n % 5 == 0)  then array << :buzz
+    elsif (n % 3 == 0)  then array << :fizz
+    else  array << n
     end
   end
   array
--- week0/1-standard-types/solution_test.rb
+++ week0/1-standard-types/solution_test.rb
@@ -4,79 +4,85 @@ require_relative 'solution'
 
 class SolutionTest < Minitest::Unit::TestCase
   def test_histograms
-    assert_equal histogram('abraca'), 'a' => 3, 'b' => 1, 'r' => 1, 'c' => 1
-    assert_equal histogram('aaaaaa'), 'a' => 6
-    assert_equal histogram('a3!3!?'), 'a' => 1, '3' => 2, '!' => 2, '?' => 1
+    abraca = { 'a' => 3, 'b' => 1, 'r' => 1, 'c' => 1 }
+    aaaaaa = { 'a' => 6 }
+    a3 = { 'a' => 1, '3' => 2, '!' => 2, '?' => 1 }
+    assert_equal abraca, histogram('abraca')
+    assert_equal aaaaaa, histogram('aaaaaa')
+    assert_equal a3, histogram('a3!3!?')
   end
 
   def test_primes
-    assert_equal prime?(7), true
-    assert_equal prime?(8), false
-    assert_equal prime?(1), false
+    assert_equal true, prime?(7)
+    assert_equal false, prime?(8)
+    assert_equal false, prime?(1)
   end
 
   def test_ordinals
-    assert_equal ordinal(2), '2nd'
-    assert_equal ordinal(112), '112th'
-    assert_equal ordinal(53), '53rd'
-    assert_equal ordinal(1054), '1054th'
-    assert_equal ordinal(16), '16th'
-    assert_equal ordinal(9), '9th'
-    assert_equal ordinal(213), '213th'
-    assert_equal ordinal(101), '101st'
-    assert_equal ordinal(1000), '1000th'
-    assert_equal ordinal(0), '0th'
+    assert_equal '2nd', ordinal(2)
+    assert_equal '112th', ordinal(112)
+    assert_equal '53rd', ordinal(53)
+    assert_equal '1054th', ordinal(1054)
+    assert_equal '16th', ordinal(16)
+    assert_equal '9th', ordinal(9)
+    assert_equal '213th', ordinal(213)
+    assert_equal '101st', ordinal(101)
+    assert_equal '1000th', ordinal(1000)
+    assert_equal '0th', ordinal(0)
   end
 
   def test_palindromes
-    assert_equal palindrome?(12_321), true
-    assert_equal palindrome?('Race car'), true
-    assert_equal palindrome?('Azobichammachiboza'), false
-    assert_equal palindrome?(293_821), false
-    assert_equal palindrome?(:racecar), true
-    # assert_equal palindrome?(0b10001), true
-    # assert_equal palindrome?(010001), true
-    # assert_equal palindrome?(0x10001), true
-    assert_equal palindrome?("Kara'o'arak"), true
+    assert_equal true, palindrome?(12_321)
+    assert_equal true, palindrome?('Race car')
+    assert_equal false, palindrome?('Azobichammachiboza')
+    assert_equal false, palindrome?(293_821)
+    assert_equal true, palindrome?(:racecar)
+    assert_equal true, palindrome?("Kara'o'arak")
   end
 
   def test_anagrams
-    assert_equal anagram?('silent', 'listen'), true
-    assert_equal anagram?('Mr Mojo Risin', 'Jim Morisson'), false
-    assert_equal anagram?('a lia', 'aila'), true
-    assert_equal anagram?('ankh', 'morpork'), false
-    # assert_equal anagram?("K'vatch", "vatchk"), false
+    assert_equal true, anagram?('silent', 'listen')
+    assert_equal false, anagram?('Mr Mojo Risin', 'Jim Morisson')
+    assert_equal true, anagram?('a lia', 'aila')
+    assert_equal false, anagram?('ankh', 'morpork')
   end
 
   def test_removing_prefixes
-    assert_equal remove_prefix('Ladies Night Out', 'Ladies '), 'Night Out'
-    assert_equal remove_prefix('Ladies Night Out', 'Ladies Night'), ' Out'
-    assert_equal remove_prefix('03453456', '034'), '53456'
-    # assert_equal remove_prefix('    _345', '  '), "  _345"
+    assert_equal 'Night Out', remove_prefix('Ladies Night Out', 'Ladies ')
+    assert_equal ' Out', remove_prefix('Ladies Night Out', 'Ladies Night')
+    assert_equal '53456', remove_prefix('03453456', '034')
+    assert_equal nil, remove_prefix('Ladies Night Out', 'morriss')
   end
 
   def test_removing_suffixes
+    ladies = 'Ladies Night Ladies'
     assert_equal 'Ladies', remove_suffix('Ladies Night Out', ' Night Out')
-    assert_equal remove_suffix('Ladies Night Ladies Out', ' Out'), 'Ladies Night Ladies'
-    assert_equal remove_suffix('1343=_433t', '33t'), '1343=_4'
-    assert_equal remove_suffix('234   _43<   __   ', '_   '), '234   _43<   _'
+    assert_equal ladies, remove_suffix('Ladies Night Ladies Out', ' Out')
+    assert_equal '1343=_4', remove_suffix('1343=_433t', '33t')
+    assert_equal '234   _43<   _', remove_suffix('234   _43<   __   ', '_   ')
+    assert_equal nil, remove_suffix('Ladies Night Out', ' catalan')
   end
 
   def test_digits
-    assert_equal digits(12_345), [1, 2, 3, 4, 5]
-    # assert_equal digits(-12345), [1, 2, 3, 4, 5]
+    assert_equal [1, 2, 3, 4, 5], digits(12_345)
   end
 
   def test_fizzbuzz
-    assert_equal fizzbuzz(1..7), [1, 2, :fizz, 4, :buzz, :fizz, 7]
+    assert_equal [1, 2, :fizz, 4, :buzz, :fizz, 7], fizzbuzz(1..7)
   end
 
   def test_count
-    assert_equal count([1, 2, 3, 1]), 1 => 2, 2 => 1, 3 => 1
-    assert_equal count(%w(an array of words words)), 'an' => 1, 'array' => 1, 'of' => 1, 'words' => 2
+    numbers = { 1 => 2, 2 => 1, 3 => 1 }
+    words = { 'an' => 1, 'array' => 1, 'of' => 1, 'words' => 2 }
+    assert_equal numbers, count([1, 2, 3, 1])
+    assert_equal words, count(%w(an array of words words))
   end
 
   def test_count_words
-    assert_equal count_words("This isn't a sentence, bro, is it?", 'Bro, this is a sentence.'), 'this' => 2, "isn't" => 1, 'a' => 2, 'sentence' => 2, 'bro' => 2, 'is' => 2, 'it' => 1
+    counts = { 'this' => 2, "isn't" => 1, 'a' => 2 }
+      .merge('sentence' => 2, 'bro' => 2, 'is' => 2, 'it' => 1)
+    first_string  = "This isn't a sentence, bro, is it?"
+    second_string = 'Bro, this is a sentence.'
+    assert_equal counts, count_words(first_string, second_string)
   end
 end
