179af546755850c48e6e1501049275f67e9d00bd
svetlozaurus <svetlik@gmail.com>
2014-11-27 21:35:48 +0200

    Refactor non-bang methods

--- week1/2-hashes/solution.rb
+++ week1/2-hashes/solution.rb
@@ -1,35 +1,30 @@
 class Hash
   def pick(*keys)
-    new_hash = dup
-    new_hash.select { |key| keys.include? key }
+    dup.pick! keys
   end
 
   def except(*keys)
-    new_hash = dup
-    new_hash.reject { |key| keys.include? key }
+    dup.except! keys
   end
 
   def compact_values
-    new_hash = dup
-    new_hash.select { |key| (new_hash[key] != false) && (new_hash[key] != nil) }
+    dup.compact_values!
   end
 
   def defaults(hash)
-    new_hash = dup
-    hash.each { |key, value| new_hash[key] = value unless new_hash.include? key }
-    new_hash
+    dup.defaults!(hash)
   end
 
   def pick!(*keys)
-    select! { |key| keys.include? key }
+    select! { |key| keys.flatten.include? key }
   end
 
   def except!(*keys)
-    reject! { |key| keys.include? key }
+    reject! { |key| keys.flatten.include? key }
   end
 
   def compact_values!
-    select! { |key| (self[key] != false) && (self[key] != nil) }
+    select! { |key| self[key] }
   end
 
   def defaults!(hash)
