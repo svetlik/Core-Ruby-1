11c4052ae0699d4ea24d7ca409314ce6d5cc626f
svetlozaurus <svetlik@gmail.com>
2014-11-28 20:32:28 +0200

    Removed an accessor from the Vector3D class

--- week2/1-vectors/vector3_solution.rb
+++ week2/1-vectors/vector3_solution.rb
@@ -1,7 +1,7 @@
 require_relative 'vector_solution'
 
 class Vector3D < Vector
-  attr_accessor :x, :y, :z, :components
+  attr_accessor :x, :y, :z
   def initialize(x, y, z)
     @components = [x, y, z]
   end
